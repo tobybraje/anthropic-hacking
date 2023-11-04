c198a5bf871c2c34b7e7eca958ea638f5dae664e
tobybraje <tobiasbraje@gmail.com>
2023-11-04 16:26:15 +0000

    Caveman mode basic chat implementation w claude 2

--- utils.py
+++ utils.py
@@ -1,2 +1,20 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from settings import settings
+
+
 def generate_response(messages):
-    pass
+    anthropic = Anthropic(api_key=settings.CLAUDE_API_KEY)
+    prompt = ""
+    for message in messages:
+        if message["role"] == "assistant":
+            prompt += f"{AI_PROMPT}{message['content']}"
+        else:
+            prompt += f"{HUMAN_PROMPT}{message['content']}"
+
+    prompt += "\n\nAssistant:"
+    completion = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=300,
+        prompt=prompt,
+    )
+    return completion.completion
